8fd50ae48be7ebcd33f7fe0b301ca9031b84e937
Matt Clark <reddevdesign@gmail.com>
2022-03-16 20:33:36 -0400

    set up .env, added colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 module.exports = {
-  mode: "jit",
+  mode: 'jit',
   theme: {
     container: {
       center: true,
     },
+    extend: {
+      fontFamily: {
+        display: ['EB Garamond', 'Hind'],
+      },
+      colors: {
+        primaryLight: '#138D7B',
+        primaryDark: '#40504F',
+        secondary: '#23CEA0',
+        accent: '#EF6B3',
+        black: '#1D1D1D',
+      },
+    },
   },
-};
+}
