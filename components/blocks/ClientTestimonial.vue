e59a22611482789b0aef9b9b8a102412eaa98047
Matt Clark <reddevdesign@gmail.com>
2022-04-20 15:02:57 -0400

    Built client-testimonial component

--- components/blocks/ClientTestimonial.vue
+++ components/blocks/ClientTestimonial.vue
@@ -1,5 +1,21 @@
 <template>
-  <section> </section>
+  <section class="max-w-screen-sm mx-auto px-4 mt-32">
+    <div class="p-4 border-2 border-secondary rounded-xl">
+      <div class="px-2 lg:px-8 pt-4 flex flex-col items-center">
+        <div class="bg-accent-light rounded-full p-5 drop-shadow-xl -mt-20">
+          <BaseIcon class="text-white" file="quote-icon" alt="Quote Icon" />
+        </div>
+
+        <BaseHeading size="h3" class="py-4"> {{ block.heading }} </BaseHeading>
+
+        <BaseText class="font-body text-lg"> <i>" </i>{{ block.quote }}<i>"</i> </BaseText>
+
+        <BaseText class="font-display text-sm font-bold pt-2 -mr-2 place-self-end">
+          - {{ block.name }}
+        </BaseText>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -12,5 +28,3 @@
     },
   }
 </script>
-
-<style lang="scss" scoped></style>
