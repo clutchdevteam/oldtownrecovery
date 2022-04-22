55898383688f5bb54d739259018bdcb607949698
Matt Clark <reddevdesign@gmail.com>
2022-04-22 15:18:43 -0400

    footer and pre-footer

--- components/global/SiteFooter.vue
+++ components/global/SiteFooter.vue
@@ -1,25 +1,48 @@
 <template>
-  <footer class="bg-primary-dark">
-    <div class="container mx-auto flex flex-col justify-between items-center py-20">
-      <div>
-        <BaseImage
-          class="w-56 h-auto"
-          :src="footerContent.logo.filename"
-          :alt="footerContent.logo.alt"
-        />
-      </div>
+  <div class="flex flex-col mt-16">
+    <div class="bg-primary-light">
+      <div
+        class="desktop-container flex flex-col lg:flex-row lg:justify-between lg:items-center py-4 lg:py-0"
+      >
+        <BaseText class="text-white font-display font-medium text-lg opacity-100 py-4 lg:py-3">
+          {{ footerContent.preFooter.text }}
+        </BaseText>
 
-      <nav class="pt-8">
-        <ul class="flex space-x-10">
-          <li v-for="menu in footerContent.footerNav" :key="menu.id">
-            <BaseLink :href="menu.slug.cached_url" class="text-white opacity-75 border-none">{{
-              menu.title
-            }}</BaseLink>
-          </li>
-        </ul>
-      </nav>
+        <div class="py-4 lg:py-3">
+          <BaseButton
+            :href="footerContent.preFooter.button[0].link.cached_url"
+            class="flex justify-center"
+          >
+            {{ footerContent.preFooter.button[0].btnText }}
+          </BaseButton>
+        </div>
+      </div>
     </div>
-  </footer>
+
+    <footer class="bg-primary-dark">
+      <div
+        class="desktop-container flex flex-col items-center lg:flex-row lg:justify-between py-12 lg:py-2"
+      >
+        <div class="py-5">
+          <BaseImage
+            class="w-56 h-auto lg:w-28"
+            :src="footerContent.logo.filename"
+            :alt="footerContent.logo.alt"
+          />
+        </div>
+
+        <nav class="w-full lg:w-auto py-5">
+          <ul class="flex justify-between md:justify-around lg:space-x-12">
+            <li v-for="menu in footerContent.footerNav" :key="menu.id">
+              <BaseLink :href="menu.slug.cached_url" class="text-white opacity-75 border-none">{{
+                menu.title
+              }}</BaseLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -32,3 +55,9 @@
     },
   }
 </script>
+
+<style lang="postcss" scoped>
+  .nuxt-link-exact-active {
+    @apply opacity-100;
+  }
+</style>
